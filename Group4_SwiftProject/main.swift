a740ee9b13d56b1ce9fc4517d120fa867ff8fbe2
MacStudent <macstudent@20x-imacxx-m-468.local>
2018-07-27 15:20:50 -0400

    Initial Commit

---
+++ Group4_SwiftProject/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Group4_SwiftProject
+//
+//  Created by MacStudent on 2018-07-27.
+//  Copyright © 2018 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
